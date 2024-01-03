f08450f51e32dc7e13d90a4f64cddf55a8c81e10
NegoSchnee <420470299@qq.com>
2024-01-03 06:46:54 -0800

    lab1 find

--- user/find.c
+++ user/find.c
@@ -3,7 +3,52 @@
 #include "kernel/types.h"
 #include "kernel/stat.h"
 #include "user/user.h"
+#include "kernel/fs.h"
 
+
+void find(char* tar, char* path) {
+    // 以只读方式打开文件
+    char *p = path;
+    for (p = path + strlen(path) - 1; p >= path && *p != '/'; p--) {}
+    p++;    // 从p开始往后读，是当前文件名
+    int fd;
+    if ((fd = open(path, 0)) < 0) {    // 读文件时需要传入：从初始目录开始的完整路径，而不是当前文件名
+        fprintf(2, "find: cannot open %s\n", path);
+        return;
+    }
+    struct stat st;    // 结构体定义在 kernel/stat.h
+    if (fstat(fd, &st) < 0) {
+        fprintf(2, "find: cannot stat %s\n", path);
+        close(fd);  // 退出前，关闭文件描述符
+        return;
+    }
+    
+    switch (st.type) {
+    case T_FILE:    // 不是目录文件，直接判断匹配并返回
+        if (strcmp(tar, p) == 0)
+            fprintf(1, "%s\n", path);
+        break;
+    case T_DIR:    // 是目录文件，递归查找
+        if (strcmp(tar, p) == 0)
+            fprintf(1, "%s\n", path);
+        // 如果冒号后面直接跟声明，必须在前面加个分号，否则报错 a label can only be part of a statement and a declaration is not a statement
+        ;struct dirent de;    // 定义在 /kernel/fs.h
+        int len0 = strlen(path);
+        while(read(fd, &de, sizeof(de)) == sizeof(de)) {    // 遍历子文件
+            if (de.inum == 0 || strcmp(de.name, "..") == 0 || strcmp(de.name, ".") == 0)
+                continue;
+            char* p1 = path + len0;
+            *p1++ = '/';
+            int lendn = strlen(de.name);
+            for (int i = 0; i < lendn; i++, p1++)
+                *p1 = de.name[i];
+            *p1 = '\0';    // 最后需要加结束符，否则之前写的文件名可能会续上
+            find(tar, path);
+        }
+        break;
+    }
+    close(fd);  // 最后退出前，关闭文件描述符
+}
 
 int main(int argc, char* argv[]) {
     // 参数不足
@@ -16,6 +61,12 @@ int main(int argc, char* argv[]) {
         fprintf(2, "find输入参数数目过多...\n");
         exit(1);
     }
+    // 要递归查找的目录
+    char path[512] = {0};
+    strcpy(path, argv[1]);
+    // 目标文件
+    char* tar = argv[2];
+    find(tar, path);
 
     exit(0);
 }
--- user/ls.c
+++ user/ls.c
@@ -4,13 +4,13 @@
 #include "kernel/fs.h"
 
 char*
-fmtname(char *path)
+fmtname(char *path)    // 只输出子文件名字，用'/'作为分隔符倒着找出最后的文件名
 {
-  static char buf[DIRSIZ+1];
+  static char buf[DIRSIZ+1];  // 宏定义，DIRSIZ值为14，规定了文件名最大为14字节 在kernel/fs.h 
   char *p;
 
   // Find first character after last slash.
-  for(p=path+strlen(path); p >= path && *p != '/'; p--)
+  for(p=path+strlen(path); p >= path && *p != '/'; p--)    // 倒着往前找到'/'，再++，从而找到当前文件名称开始的位置
     ;
   p++;
 
@@ -18,7 +18,7 @@ fmtname(char *path)
   if(strlen(p) >= DIRSIZ)
     return p;
   memmove(buf, p, strlen(p));
-  memset(buf+strlen(p), ' ', DIRSIZ-strlen(p));
+  memset(buf+strlen(p), ' ', DIRSIZ-strlen(p));    // 不够长的部分用空格填充
   return buf;
 }
 
@@ -27,15 +27,15 @@ ls(char *path)
 {
   char buf[512], *p;
   int fd;
-  struct dirent de;
-  struct stat st;
+  struct dirent de;    // 定义在 /kernel/fs.h
+  struct stat st;    // 定义在 kernel/stat.h      如何找到：linux在xv6-lab-2020文件夹下运行命令   grep -rn "struct stat" .
 
-  if((fd = open(path, 0)) < 0){
+  if((fd = open(path, 0)) < 0){    // 以只读方式打开文件
     fprintf(2, "ls: cannot open %s\n", path);
     return;
   }
 
-  if(fstat(fd, &st) < 0){
+  if(fstat(fd, &st) < 0){    // 把文件状态传入结构体st中
     fprintf(2, "ls: cannot stat %s\n", path);
     close(fd);
     return;
@@ -47,19 +47,19 @@ ls(char *path)
     break;
 
   case T_DIR:
-    if(strlen(path) + 1 + DIRSIZ + 1 > sizeof buf){
+    if(strlen(path) + 1 + DIRSIZ + 1 > sizeof buf){   // 路径/文件名'0'  加起来不能超过buf大小
       printf("ls: path too long\n");
       break;
     }
     strcpy(buf, path);
-    p = buf+strlen(buf);
+    p = buf+strlen(buf);  // buf存当前目录名
     *p++ = '/';
-    while(read(fd, &de, sizeof(de)) == sizeof(de)){
+    while(read(fd, &de, sizeof(de)) == sizeof(de)){    // 遍历读目录文件中的内容，打印里面的文件信息
       if(de.inum == 0)
         continue;
-      memmove(p, de.name, DIRSIZ);
-      p[DIRSIZ] = 0;
-      if(stat(buf, &st) < 0){
+      memmove(p, de.name, DIRSIZ);  // 把子文件名续到buf中的 当前目录名/后面
+      p[DIRSIZ] = 0;  // 标识结束。后面函数读到这里就会停
+      if(stat(buf, &st) < 0){    // 路径名：包含当前路径，来调用stat()函数
         printf("ls: cannot stat %s\n", buf);
         continue;
       }
@@ -67,7 +67,7 @@ ls(char *path)
     }
     break;
   }
-  close(fd);
+  close(fd);  // 最后注意关掉文件描述符
 }
 
 int
@@ -79,7 +79,7 @@ main(int argc, char *argv[])
     ls(".");
     exit(0);
   }
-  for(i=1; i<argc; i++)
+  for(i=1; i<argc; i++)   // 遍历参数，ls命令支持列举多个路径下的文件
     ls(argv[i]);
   exit(0);
 }
